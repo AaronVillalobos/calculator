c870b7b3cd8f80634d14ac3f21b451be120c81c6
AaronVillalobos <avilla4537@gmail.com>
2024-07-12 17:53:19 -0700

    Write functions for math and calculator operations

---
+++ script.js
@@ -0,0 +1,34 @@
+/* Math Operators */
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+/* Calculator Operations */
+function operation(num1, operator, num2) {
+    switch(operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            console.log("Invalid Operation");
+    }
+}
+
+console.log(operation(15, '/', 15));
